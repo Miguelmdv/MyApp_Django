70ba72b54a9f2ef8de27ea6c3ac3985495906d00
Miguel Angel Madrid <m.angelmadrid@gmail.com>
2023-11-01 13:49:23 +0100

    get method

--- myapp/models.py
+++ myapp/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class Proyect(models.Model):
+class Project(models.Model):
     name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
     
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    proyect = models.ForeignKey(Proyect, on_delete=models.CASCADE)
+    proyect = models.ForeignKey(Project, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title + " - " + self.proyect.name
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h1>Hello World</h1>")    
+def index(request):
+    return HttpResponse("Index Page")
+
+def hello(request, username):
+    return HttpResponse(f"<h1>Hello {username}</h1>")    
 
 def about(request):
     return HttpResponse("About")
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def tasks(request, id):
+    task = get_object_or_404(Task, id=id)
+    return HttpResponse(f"Tasks: {task.title}")
