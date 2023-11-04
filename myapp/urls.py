27a9bc62278e7d5577d6d765959c2a2bbdac5142
Miguel Angel Madrid <m.angelmadrid@gmail.com>
2023-11-04 23:13:37 +0100

    LogIn

--- myapp/urls.py
+++ myapp/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     path("create_task/", views.create_task, name="create_task"),
     path("projects", views.delete_project, name="delete_project"),
     path("task", views.update_delete_task, name="update_delete_task"),
+    path("signup/", views.signup, name="sign_up"),
+    path("logout/", views.signout, name="log_out"),
+    path("login/", views.signin, name="log_in"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
+from django.http import HttpResponse
 from .models import Project, Task
 from django.shortcuts import get_object_or_404
-from .forms import CreateNewTask, CreateNewProject
+from .forms import CreateNewTask, CreateNewProject, CustomUserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
 
 
 # Create your views here.
@@ -27,8 +31,6 @@ def projects(request):
 
 
 def tasks(request):
-    # task = get_object_or_404(Task, id=id)
-    # return HttpResponse(f"Tasks: {task.title}")
     tasks = Task.objects.all()
     return render(request, "tasks/tasks.html", {"tasks": tasks})
 
@@ -41,9 +43,7 @@ def create_project(request):
         )
     else:
         # Save data
-        Project.objects.create(
-            name=request.POST["name"]
-        )
+        Project.objects.create(name=request.POST["name"])
         return redirect("projects")
 
 
@@ -60,13 +60,13 @@ def create_task(request):
         )
         return redirect("tasks")
 
+
 def project_detail(request, id):
     project = get_object_or_404(Project, id=id)
     tasks = Task.objects.filter(project=id)
-    return render(request, "projects/detail.html", {
-        "project": project, "tasks": tasks
-    })
-    
+    return render(request, "projects/detail.html", {"project": project, "tasks": tasks})
+
+
 def delete_project(request):
     if request.method == "POST":
         if "project_id" in request.POST:
@@ -74,25 +74,66 @@ def delete_project(request):
             if project_id:
                 project = Project.objects.get(id=project_id)
                 project.delete()
-                
-    previous_url = request.META.get('HTTP_REFERER', 'projects')
+
+    previous_url = request.META.get("HTTP_REFERER", "projects")
     return redirect(previous_url)
-    
+
+
 def update_delete_task(request):
     if request.method == "POST":
         if "check_task" in request.POST:
             task_id = request.POST["check_task"]
             task = Task.objects.get(id=task_id)
             task.done = not task.done
-            task.save()       
+            task.save()
         elif "task_id" in request.POST:
             task_id = request.POST["task_id"]
             if task_id:
                 task = Task.objects.get(id=task_id)
                 task.delete()
 
-    previous_url = request.META.get('HTTP_REFERER', 'tasks')
+    previous_url = request.META.get("HTTP_REFERER", "tasks")
     return redirect(previous_url)
 
 
+def signup(request):
+    context = {"form": UserCreationForm(), "error_message": ""}
+    if request.method == "POST":
+        form = request.POST
+        if form["password1"] == form["password2"]:
+            try:
+                user = User.objects.create_user(
+                    form["username"], password=form["password1"]
+                )
+                user.save()
+                login(request, user)
+                return redirect("projects")
+            except IntegrityError:
+                context["error_message"] = "Username already exists"
+                return render(request, "sign/signup.html", context)
+        else:
+            context["error_message"] = "Passwords don't match"
+            return render(request, "sign/signup.html", context)
+    else:
+        return render(request, "sign/signup.html", context)
 
+
+def signout(request):
+    logout(request)
+    return redirect("index")
+
+
+def signin(request):
+    context = {"form": AuthenticationForm(), "error_message": ""}
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            context["error_message"] = "User or password is incorrect"
+            return render(request, "sign/login.html", context)
+        else:
+            login(request, user)
+            return redirect("projects")
+    else:
+        return render(request, "sign/login.html", context)
